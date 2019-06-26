de33d42bcd2b0e93087aa24a84fc787cfe3507f4
Eugene Pogrebnyak <emcifuntik@gmail.com>
2019-06-26 16:35:52 +0300

    Added player weapon methods
    IP Address getter
    Armour & health setters
    Getter for entity that player aim at and aim offset

--- entities/IPlayer.h
+++ entities/IPlayer.h
@@ -12,6 +12,7 @@ namespace alt
 		virtual bool IsConnected() const = 0;
 
 		virtual uint32_t GetPing() const = 0;
+		virtual StringView GetIP() const = 0;
 
 		virtual void Spawn(Position pos, uint32_t delayMs) = 0;
 		virtual void Despawn() = 0;
@@ -21,6 +22,8 @@ namespace alt
 
 		virtual uint16_t GetHealth() const = 0;
 		virtual void SetHealth(uint16_t health) = 0;
+		virtual uint16_t GetMaxHealth() const = 0;
+		virtual void SetMaxHealth(uint16_t health) = 0;
 
 		virtual void SetDateTime(int day, int month, int year, int hour, int minute, int second) = 0;
 		virtual void SetWeather(uint32_t weather) = 0;
@@ -28,6 +31,16 @@ namespace alt
 		virtual void GiveWeapon(uint32_t weapon, int32_t ammo, bool selectWeapon) = 0;
 		virtual void RemoveWeapon(uint32_t weapon) = 0;
 		virtual void RemoveAllWeapons() = 0;
+
+		virtual void AddWeaponComponent(uint32_t weapon, uint32_t component) = 0;
+		virtual void RemoveWeaponComponent(uint32_t weapon, uint32_t component) = 0;
+		virtual alt::Array<uint32_t> GetCurrentWeaponComponents() const = 0;
+
+		virtual void SetWeaponTintIndex(uint32_t weapon, uint8_t tintIndex) = 0;
+		virtual uint8_t GetCurrentWeaponTintIndex() const = 0;
+
+		virtual uint32_t GetCurrentWeapon() const = 0;
+		virtual void SetCurrentWeapon(uint32_t weapon) = 0;
 
 		virtual bool IsDead() const = 0;
 
@@ -37,8 +50,10 @@ namespace alt
 		virtual bool IsShooting() const = 0;
 		virtual bool IsReloading() const = 0;
 
-		virtual uint16_t GetArmor() const = 0;
-		virtual void SetArmor(uint16_t armor) = 0;
+		virtual uint16_t GetArmour() const = 0;
+		virtual void SetArmour(uint16_t armor) = 0;
+		virtual uint16_t GetMaxArmour() const = 0;
+		virtual void SetMaxArmour(uint16_t armor) = 0;
 
 		virtual float GetMoveSpeed() const = 0;
 
@@ -51,6 +66,9 @@ namespace alt
 		virtual bool IsInVehicle() const = 0;
 		virtual IVehicle* GetVehicle() const = 0;
 		virtual uint8_t GetSeat() const = 0;
+
+		virtual IEntity* GetEntityAimingAt() const = 0;
+		virtual Position GetEntityAimOffset() const = 0;
 
 		virtual void Kick(StringView reason = "kicked out") = 0;
 
