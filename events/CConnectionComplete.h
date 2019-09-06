5b011878a21163e00ddd8a8424324c44e992f296
VadZz <vadzz@vadzz.dev>
2019-09-07 02:35:04 +0300

    Removed isAnyMapLoaded parameter from CConnectionCompleteEvent

--- events/CConnectionComplete.h
+++ events/CConnectionComplete.h
@@ -9,16 +9,10 @@ namespace alt
 	class CConnectionCompleteEvent : public CEvent
 	{
 	public:		
-		CConnectionCompleteEvent(bool _anyMapLoaded) :
-			CEvent(Type::CONNECTION_COMPLETE),
-			isAnyMapLoaded(_anyMapLoaded)
+		CConnectionCompleteEvent() :
+			CEvent(Type::CONNECTION_COMPLETE)
 		{
 
 		}
-
-		bool IsAnyMapLoaded() const { return isAnyMapLoaded; }
-
-	private:
-		bool isAnyMapLoaded;
 	};
 }
