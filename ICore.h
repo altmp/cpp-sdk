e052291b301cefacbad9008bd9ed12a2cdee3324
Eugene Pogrebnyak <emcifuntik@live.com>
2019-12-27 18:08:58 +0300

    SDK version increment

--- ICore.h
+++ ICore.h
@@ -29,7 +29,7 @@ namespace alt
 	class ICore
 	{
 	public:
-		static constexpr uint32_t SDK_VERSION = 21;
+		static constexpr uint32_t SDK_VERSION = 22;
 
 		// Shared methods
 		virtual void LogInfo(StringView str) = 0;
