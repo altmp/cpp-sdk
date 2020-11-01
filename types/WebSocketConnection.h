0d2b74f269132345349a6a42c9857dea393e43a0
fabian <fabian.terhorst@gmail.com>
2020-11-02 00:43:43 +0100

    Fix code style

--- types/WebSocketConnection.h
+++ types/WebSocketConnection.h
@@ -4,31 +4,33 @@
 
 namespace alt {
 
-    using WebSocketStateCallback = void (*)(WebSocketConnection *WebSocketConnection, State oldState,
-                                            State newState);
-    using WebSocketMessageCallback = void (*)(WebSocketConnection *WebSocketConnection, uint8_t *data);
+	using WebSocketStateCallback = void (*)(WebSocketConnection *WebSocketConnection, State oldState,
+											State newState);
+	using WebSocketMessageCallback = void (*)(WebSocketConnection *WebSocketConnection, uint8_t *data);
 
-    class WebSocketConnection {
-    public:
-        enum class State : uint8_t {
-            Connecting,
-            Open,
-            Closing,
-            Closed
-        };
+	class WebSocketConnection
+	{
+	public:
+		enum class State : uint8_t
+		{
+			Connecting,
+			Open,
+			Closing,
+			Closed
+		};
 
-        virtual ~WebSocketConnection() = default;
+		virtual ~WebSocketConnection() = default;
 
-        State GetState() const = 0;
+		State GetState() const = 0;
 
-        void Open() const = 0;
+		void Open() const = 0;
 
-        void Close(int code) const = 0;
+		void Close(int code) const = 0;
 
-        void Send(uint8_t *data) const = 0;
+		void Send(uint8_t *data) const = 0;
 
-        void AddStateListener(WebSocketStateCallback cb) const = 0;
+		void AddStateListener(WebSocketStateCallback cb) const = 0;
 
-        void AddMessageListener(WebSocketMessageCallback cb) const = 0;
-    }
+		void AddMessageListener(WebSocketMessageCallback cb) const = 0;
+	}
 }
