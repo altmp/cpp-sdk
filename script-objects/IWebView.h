42abb45ea4c77ea1c2d20efb823c8c09c3a41bdb
Hazard <hazard9909@gmail.com>
2020-11-03 14:01:04 +0100

    Made some getter const in webview

--- script-objects/IWebView.h
+++ script-objects/IWebView.h
@@ -9,12 +9,15 @@ namespace alt
     {
     public:
         virtual ~IWebView() = default;
-        virtual void Trigger(alt::StringView eventname, alt::MValueArgs &args) = 0;
+        virtual void Trigger(alt::StringView eventname, const alt::MValueArgs &args) = 0;
         virtual void Focus() = 0;
         virtual void Unfocus() = 0;
-        virtual String GetUrl() = 0;
+        virtual bool IsFocused() const = 0;
+        virtual String GetUrl() const = 0;
         virtual void SetUrl(alt::StringView url) = 0;
-        virtual bool IsVisible() = 0;
+        virtual bool IsVisible() const = 0;
         virtual void SetVisible(bool visible) = 0;
+        virtual bool IsOverlay() const = 0;
+        virtual bool IsReady() const = 0;
     };
 } // namespace alt
