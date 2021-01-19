5014a1b7912ee43782ad9f2c9b63a456bc758638
fabian <fabian.terhorst@gmail.com>
2021-01-19 21:09:53 +0100

    Fix drawable type

--- objects/IPlayer.h
+++ objects/IPlayer.h
@@ -78,7 +78,7 @@ namespace alt
 		virtual void SetWeather(uint32_t weather) = 0;
 		virtual void Kick(StringView reason = "Kicked") = 0;
 		virtual alt::Cloth GetClothes(uint8_t component) const = 0;
-		virtual void SetClothes(uint8_t component, uint8_t drawable, uint8_t texture, uint8_t palette) = 0;
+		virtual void SetClothes(uint8_t component, uint16_t drawable, uint8_t texture, uint8_t palette) = 0;
 #else
 		virtual bool IsTalking() const = 0;
 		virtual float GetMicLevel() const = 0;
--- types/Cloth.h
+++ types/Cloth.h
@@ -7,13 +7,13 @@ namespace alt
 {
 	struct Cloth
 	{
-		uint8_t drawableId = 0;
+		uint16_t drawableId = 0;
 		uint8_t textureId = 0;
 		uint8_t paletteId = 0;
 
 		Cloth() = default;
 
-		Cloth(uint8_t _drawableId, uint8_t _textureId, uint8_t _paletteId) :
+		Cloth(uint16_t _drawableId, uint8_t _textureId, uint8_t _paletteId) :
 				drawableId(_drawableId), textureId(_textureId), paletteId(_paletteId) { }
 
 		friend std::ostream &operator<<(std::ostream &stream, const Cloth &cloth)
