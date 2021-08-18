df77829c9ba16610014e74cbbcb44dc5fd00ef94
fabian <fabian.terhorst@gmail.com>
2021-08-18 23:19:03 +0200

    Add armour damage to player damage event

--- events/CPlayerDamageEvent.h
+++ events/CPlayerDamageEvent.h
@@ -13,11 +13,12 @@ namespace alt
 	class CPlayerDamageEvent : public CEvent
 	{
 	public:
-		CPlayerDamageEvent(Ref<IPlayer> _target, Ref<IEntity> _attacker, uint16_t _damage, uint32_t _weapon) :
+		CPlayerDamageEvent(Ref<IPlayer> _target, Ref<IEntity> _attacker, uint16_t _healthDamage, uint16_t _armourDamage, uint32_t _weapon) :
 			CEvent(Type::PLAYER_DAMAGE),
 			target(_target),
 			attacker(_attacker),
-			damage(_damage),
+			healthDamage(_healthDamage),
+			armourDamage(_armourDamage),
 			weapon(_weapon)
 		{
 
@@ -25,13 +26,15 @@ namespace alt
 
 		Ref<IPlayer> GetTarget() const { return target; }
 		Ref<IEntity> GetAttacker() const { return attacker; }
-		uint16_t GetDamage() const { return damage; }
+		uint16_t GetHealthDamage() const { return healthDamage; }
+		uint16_t GetArmourDamage() const { return armourDamage; }
 		uint32_t GetWeapon() const { return weapon; }
 
 	private:
 		Ref<IPlayer> target;
 		Ref<IEntity> attacker;
-		uint16_t damage;
+		uint16_t healthDamage;
+		uint16_t armourDamage;
 		uint32_t weapon;
 	};
 }
