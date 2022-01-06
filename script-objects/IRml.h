86b556d70d89bb86cc7142f76b73dc32c9a143f2
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-01-06 23:44:09 +0100

    Even more rmlui stuff

--- script-objects/IRml.h
+++ script-objects/IRml.h
@@ -15,7 +15,25 @@ namespace alt
         virtual bool HasClass(const std::string& name) const = 0;
         virtual const std::vector<std::string> GetClassList() const = 0;
 
-        virtual void SetOffset(Ref<IRmlElement> offsetParent, alt::Vector2f offset, bool fixed = false) = 0;
+        virtual void SetOffset(Ref<IRmlElement> offsetParent, Vector2f offset, bool fixed = false) = 0;
+        virtual Vector2f GetRelativeOffset() const = 0;
+        virtual Vector2f GetAbsoluteOffset() const = 0;
+        virtual float GetBaseline() const = 0;
+
+        virtual bool IsPointWithinElement(Vector2f point) const = 0;
+
+        virtual float GetZIndex() const = 0;
+
+        virtual bool SetProperty(const std::string& name, const std::string& value) = 0;
+        virtual bool RemoveProperty(const std::string& name) = 0;
+        // Checks property for local element and all inherited from ancestors
+        virtual bool HasProperty(const std::string& name) const = 0;
+        // Only check property for local element
+        virtual bool HasLocalProperty(const std::string& name) const = 0;
+        virtual const std::string& GetProperty(const std::string& name) const = 0;
+        virtual const std::string& GetLocalProperty(const std::string& name) const = 0;
+        // Returns the relative unit (e.g. 'percent' or 'angle') as absolute value ('px' or 'rad')
+        virtual float GetPropertyAbsoluteValue(const std::string& name) const = 0;
         
         const std::type_info& GetTypeInfo() const override { return typeid(this); }
     };
