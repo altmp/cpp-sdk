87acd2f8889fd4dca698c057aca429fee5ab88ef
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-07-21 02:26:38 +0200

    Add HasWeapon and GetWeapons

--- objects/ILocalPlayer.h
+++ objects/ILocalPlayer.h
@@ -14,6 +14,8 @@ namespace alt
         virtual uint16_t GetCurrentAmmo() const = 0;
 
         virtual uint16_t GetWeaponAmmo(uint32_t weaponHash) const = 0;
+        virtual bool HasWeapon(uint32_t weaponHash) const = 0;
+        virtual std::vector<uint32_t> GetWeapons() const = 0;
 
         virtual alt::Ref<IWeaponData> GetCurrentWeaponData() const = 0;
 
