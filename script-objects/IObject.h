5772b692d2740683ea74b387b20d255eae085be7
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-08-02 16:31:06 +0200

    Remove object fire methods

--- script-objects/IObject.h
+++ script-objects/IObject.h
@@ -19,10 +19,6 @@ namespace alt
 		virtual bool HasGravity() const = 0;
 		virtual void ToggleGravity(bool toggle) = 0;
 
-		virtual bool IsOnFire() const = 0;
-		virtual uint32_t StartFire() = 0;
-		virtual void StopFire() = 0;
-
 #ifdef ALT_CLIENT_API
 		virtual bool IsRemote() const = 0;
 #endif
