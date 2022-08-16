7ceb2f5dc02c3eea1211fffc15df98ec41740c7e
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-08-16 17:34:20 +0200

    Only emit player weapon shoot event for local player

--- events/CPlayerWeaponShootEvent.h
+++ events/CPlayerWeaponShootEvent.h
@@ -10,21 +10,18 @@ namespace alt
 	class CPlayerWeaponShootEvent : public CEvent
 	{
 	public:
-		CPlayerWeaponShootEvent(Ref<IPlayer> _target, uint32_t _weapon, uint16_t _totalAmmo, uint16_t _ammoInClip) :
+		CPlayerWeaponShootEvent(uint32_t _weapon, uint16_t _totalAmmo, uint16_t _ammoInClip) :
 			CEvent(Type::PLAYER_WEAPON_SHOOT_EVENT),
-			target(_target),
 			weapon(_weapon),
 			totalAmmo(_totalAmmo),
 			ammoInClip(_ammoInClip)
 		{}
 
-		Ref<IPlayer> GetTarget() const { return target; }
 		uint32_t GetWeapon() const { return weapon; }
 		uint16_t GetTotalAmmo() const { return totalAmmo; }
 		uint16_t GetAmmoInClip() const { return ammoInClip; }
 
 	private:
-		Ref<IPlayer> target;
 		uint32_t weapon;
 		uint16_t totalAmmo;
 		uint16_t ammoInClip;
