3e0ec995533ad943619fbd2e2d78699e6254fdc7
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-09-15 23:22:57 +0200

    overload weapon change event for client

--- events/CPlayerWeaponChangeEvent.h
+++ events/CPlayerWeaponChangeEvent.h
@@ -10,21 +10,32 @@ namespace alt
 	class CPlayerWeaponChangeEvent : public CEvent
 	{
 	public:
+#ifdef ALT_SERVER_API
 		CPlayerWeaponChangeEvent(Ref<IPlayer> _target, uint32_t _oldWeapon, uint32_t _newWeapon) :
 			CEvent(Type::PLAYER_WEAPON_CHANGE),
 			target(_target),
 			oldWeapon(_oldWeapon),
 			newWeapon(_newWeapon)
-		{
+		{}
+#endif
+#ifdef ALT_CLIENT_API
+		CPlayerWeaponChangeEvent(uint32_t _oldWeapon, uint32_t _newWeapon) :
+			CEvent(Type::PLAYER_WEAPON_CHANGE),
+			oldWeapon(_oldWeapon),
+			newWeapon(_newWeapon)
+		{}
+#endif
 
-		}
-
+#ifdef ALT_SERVER_API
 		Ref<IPlayer> GetTarget() const { return target; }
+#endif
 		uint32_t GetOldWeapon() const { return oldWeapon; }
 		uint32_t GetNewWeapon() const { return newWeapon; }
 
 	private:
+#ifdef ALT_SERVER_API
 		Ref<IPlayer> target;
+#endif
 		uint32_t oldWeapon;
 		uint32_t newWeapon;
 	};
