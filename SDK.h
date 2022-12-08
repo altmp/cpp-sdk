6e02d4ce03e04e5ad266cb8ee2b0bc47de3f56d6
Fabian Terhorst <fabian.terhorst@gmail.com>
2022-12-08 23:08:49 +0100

    backward compatibility

--- SDK.h
+++ SDK.h
@@ -19,7 +19,9 @@
 #endif // _WIN32
 
 #include "deps/alt-math/alt-math.h"
-#include "deps/alt-config/alt-config.h"
+#if __cplusplus >= 201703L
+  #include "deps/alt-config/alt-config.h"
+#endif
 
 #include "types/Types.h"
 #include "types/RGBA.h"
