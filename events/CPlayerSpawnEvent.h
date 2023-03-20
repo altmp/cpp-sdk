15fc4000d2d7d1a9057e18b9acddb9608e49b074
Fabian Terhorst <fabian.terhorst@gmail.com>
2023-03-20 18:52:16 +0100

    add player spawn event

---
+++ events/CPlayerSpawnEvent.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+#include "CEvent.h"
+
+namespace alt
+{
+	class IPlayer;
+
+	class CPlayerSpawnEvent : public CEvent
+	{
+	public:
+		CPlayerSpawnEvent(const std::shared_ptr<IPlayer>& _player) :
+			CEvent(Type::PLAYER_SPAWN),
+			player(_player),
+		{
+
+		}
+
+		IPlayer* GetPlayer() const { return player.get(); }
+
+	private:
+		std::shared_ptr<IPlayer> player;
+	};
+}
