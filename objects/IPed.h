ee33ea95686db4dd4ed0a416355a08a6952161ff
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-21 22:16:01 +0100

    add ped health, max health and current weapon

--- objects/IPed.h
+++ objects/IPed.h
@@ -10,6 +10,22 @@ namespace alt
         virtual ~IPed() = default;
 
     public:
+        virtual uint16_t GetHealth() const = 0;
+        virtual uint16_t GetMaxHealth() const = 0;
+
+        virtual uint16_t GetArmour() const = 0;
+        virtual uint16_t GetMaxArmour() const = 0;
+
+        virtual uint32_t GetCurrentWeapon() const = 0;
+        
+#ifdef ALT_SERVER_API
+        virtual void SetHealth(uint16_t health) = 0;
+        virtual void SetMaxHealth(uint16_t health) = 0;
+
+        virtual void SetArmour(uint16_t armor) = 0;
+
+        virtual void SetCurrentWeapon(uint32_t weapon) = 0;
+#endif
 
     };
 } // namespace alt
