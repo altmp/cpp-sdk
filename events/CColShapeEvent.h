c3f2af2fcda3094460d61757230f16ea314a9d86
Fabian Terhorst <fabian.terhorst@gmail.com>
2023-04-14 02:05:58 +0200

    improve CColShapeEvent

--- events/CColShapeEvent.h
+++ events/CColShapeEvent.h
@@ -6,12 +6,12 @@
 namespace alt
 {
 	class IColShape;
-	class IEntity;
+	class IWorldObject;
 
 	class CColShapeEvent : public CEvent
 	{
 	public:
-		CColShapeEvent(const std::shared_ptr<IColShape>& _target, const std::shared_ptr<IEntity>& _entity, bool _state) :
+		CColShapeEvent(const std::shared_ptr<IColShape>& _target, const std::shared_ptr<IWorldObject>& _entity, bool _state) :
 			CEvent(Type::COLSHAPE_EVENT),
 			target(_target),
 			entity(_entity),
@@ -21,12 +21,12 @@ namespace alt
 		}
 
 		IColShape* GetTarget() const { return target.get(); }
-		IEntity* GetEntity() const { return entity.get(); }
+		IWorldObject* GetEntity() const { return entity.get(); }
 		bool GetState() const { return state; }
 
 	private:
 		std::shared_ptr<IColShape> target;
-		std::shared_ptr<IEntity> entity;
+		std::shared_ptr<IWorldObject> entity;
 		bool state;
 	};
 }
