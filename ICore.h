2cb2120f11324539f0b494f9f1294175690e69a2
fabian <fabian.terhorst@gmail.com>
2023-05-26 21:04:39 +0200

    fix

--- ICore.h
+++ ICore.h
@@ -3,6 +3,7 @@
 #include <climits>
 #include <memory>
 #include <vector>
+#include <unordered_map>
 
 #include "deps/alt-math/alt-math.h"
 #include "deps/alt-config/alt-config.h"
@@ -402,7 +403,7 @@ namespace alt
 
 		virtual std::vector<IConnectionInfo*> GetConnectionInfos() const = 0;
 
-		virtual Metric* RegisterMetric(const std::string& metricName, Metric::Type metricType = Metric::Type::METRIC_TYPE_GAUGE, const std::unordered_map<std::string, std::string>& attributes = {});
+		virtual Metric* RegisterMetric(const std::string& metricName, Metric::Type metricType = Metric::Type::METRIC_TYPE_GAUGE, const std::unordered_map<std::string, std::string>& attributes = {}) = 0;
 #endif
 
 		static ICore &Instance()
