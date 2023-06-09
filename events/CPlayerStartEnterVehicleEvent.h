63166cc103fe1c8c5b350dbeb3f9723d9fcc32c4
Vektor <whiteman.chr@hotmail.com>
2023-06-09 10:47:28 +0200

    add new events

---
+++ events/CPlayerStartEnterVehicleEvent.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CEvent.h"
+
+namespace alt
+{
+	class IVehicle;
+	class IPlayer;
+
+	class CPlayerStartEnterVehicleEvent : public CEvent
+	{
+	public:
+		CPlayerStartEnterVehicleEvent(const std::shared_ptr<IVehicle>& _target, const std::shared_ptr<IPlayer>& _player, uint8_t _seat) :
+			CEvent(Type::PLAYER_START_ENTER_VEHICLE),
+			target(_target),
+			player(_player),
+			seat(_seat)
+		{
+
+		}
+
+		IVehicle* GetTarget() const { return target.get(); }
+		IPlayer* GetPlayer() const { return player.get(); }
+		uint8_t GetSeat() const { return seat; }
+
+	private:
+		std::shared_ptr<IVehicle> target;
+		std::shared_ptr<IPlayer> player;
+		uint8_t seat;
+	};
+}
---
+++ events/CPlayerStartLeaveVehicleEvent.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CEvent.h"
+
+namespace alt
+{
+	class IVehicle;
+	class IPlayer;
+
+	class CPlayerStartLeaveVehicleEvent : public CEvent
+	{
+	public:
+		CPlayerStartLeaveVehicleEvent(const std::shared_ptr<IVehicle>& _target, const std::shared_ptr<IPlayer>& _player, uint8_t _seat) :
+			CEvent(Type::PLAYER_START_LEAVE_VEHICLE),
+			target(_target),
+			player(_player),
+			seat(_seat)
+		{
+
+		}
+
+		IVehicle* GetTarget() const { return target.get(); }
+		IPlayer* GetPlayer() const { return player.get(); }
+		uint8_t GetSeat() const { return seat; }
+
+	private:
+		std::shared_ptr<IVehicle> target;
+		std::shared_ptr<IPlayer> player;
+		uint8_t seat;
+	};
+}
