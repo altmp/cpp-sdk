f56bb0de51e37cc8e3f73bf21cf10c805e556d39
Vektor <whiteman.chr@hotmail.com>
2023-07-06 12:46:51 +0200

    add filter to audio api

--- script-objects/IAudioOutput.h
+++ script-objects/IAudioOutput.h
@@ -5,6 +5,7 @@
 namespace alt
 {
 	class IAudio;
+	class IAudioFilter;
 	class IAudioOutput : public virtual IBaseObject
 	{
 	public:
@@ -13,10 +14,13 @@ namespace alt
 
 		virtual void SetMuted(bool toggle) = 0;
 		virtual bool IsMuted() const = 0;
-
-		virtual void SetCategory(uint32_t cat) = 0;
+		
 		virtual uint32_t GetCategory() const = 0;
 
 		virtual IAudio* GetOwner() const = 0;
+
+		virtual void AddFilter(IAudioFilter* filter) = 0;
+		virtual void RemoveFilter() = 0;
+		virtual IAudioFilter* GetFilter() const = 0;
 	};
 }
