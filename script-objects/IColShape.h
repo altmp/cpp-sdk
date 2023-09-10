4ac10c631e45eb81798f7b49a7bcdb0ff8a8dc05
Vektor <whiteman.chr@hotmail.com>
2023-09-10 14:07:58 +0200

    add colshape classes

--- script-objects/IColShape.h
+++ script-objects/IColShape.h
@@ -34,4 +34,41 @@ namespace alt
 		virtual void SetPlayersOnly(bool state) = 0;
 		virtual bool IsPlayersOnly() const = 0;
 	};
+
+	class IColShapeSphere : public virtual IColShape
+	{
+		virtual float GetRadius() const = 0;
+	};
+
+	class IColShapeCylinder : public virtual IColShape
+	{
+		virtual float GetRadius() const = 0;
+		virtual float GetHeight() const = 0;
+	};
+
+	class IColShapeCircle : public virtual IColShape
+	{
+		virtual float GetRadius() const = 0;
+	};
+
+	class IColShapeCuboid : public virtual IColShape
+	{
+		virtual Position GetMin() const = 0;
+		virtual Position GetMax() const = 0;
+	};
+
+	class IColShapeRect : public virtual IColShape
+	{
+		virtual Vector2f GetMin() const = 0;
+		virtual Vector2f GetMax() const = 0;
+	};
+
+	class IColShapePoly : public virtual IColShape
+	{
+		virtual float GetMinZ() const = 0;
+		virtual float GetMaxZ() const = 0;
+
+		virtual std::vector<Vector2f> GetPoints() const = 0;
+	};
+
 } // namespace alt
