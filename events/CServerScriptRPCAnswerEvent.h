f892f36baf41ca531f00cf0a6a1f342dd9a2675a
Fabian Terhorst <fabian.terhorst@gmail.com>
2023-09-24 09:19:11 +0200

    Fix clientside rpc answer event

--- events/CServerScriptRPCAnswerEvent.h
+++ events/CServerScriptRPCAnswerEvent.h
@@ -12,22 +12,19 @@ namespace alt
 	class CServerScriptRPCAnswerEvent : public CEvent
 	{
 	public:
-        CServerScriptRPCAnswerEvent(const std::shared_ptr<IPlayer>& _target, uint16_t _answerID, const MValueArgs& _answer, const std::string& _error) :
+        CServerScriptRPCAnswerEvent(uint16_t _answerID, const MValueArgs& _answer, const std::string& _error) :
 			CEvent(Type::SERVER_SCRIPT_RPC_ANSWER_EVENT),
-			target(_target),
 			answer(_answer),
 			answerError(_error)
 		{
 
 		}
 
-		IPlayer* GetTarget() const { return target.get(); }
 		uint16_t GetAnswerID() const { return answerID; }
 		MValueArgs GetAnswer() const { return answer; }
 		std::string GetAnswerError() const { return answerError; }
 
 	private:
-		std::shared_ptr<IPlayer> target;
 		uint16_t answerID;
 		MValueArgs answer;
 		std::string answerError;
