2fe9645da8d1cf8db2867cc76f189ff3c57faf89
Fabian <fabian.terhorst@gmail.com>
2024-01-03 00:00:05 +0100

    feat(shared): add count to decoration

--- types/Decoration.h
+++ types/Decoration.h
@@ -9,10 +9,11 @@ namespace alt
     public:
         uint32_t collection;
         uint32_t overlay;
+        uint8_t count;
 
         bool operator==(CDecoration other) const
         {
-            return collection == other.collection && overlay == other.overlay;
+            return collection == other.collection && overlay == other.overlay && count == other.count;
         }
     };
 }
