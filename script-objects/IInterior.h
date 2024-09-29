45a82a3ae3cbed371a46f6390db261377a59693d
Till Schreiber <till@tillschreiber.de>
2024-09-29 16:00:49 +0200

    ALTV-426 Add Interior class

---
+++ script-objects/IInterior.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <cstdint>
+#include <string>
+//#ifdef ALT_CLIENT_API
+
+namespace alt
+{
+	class IInterior
+	{
+	protected:
+		virtual ~IInterior() = default;
+
+	public:
+		struct ExtentInfo
+		{
+			alt::Position min;
+			alt::Position max;
+		};
+
+		virtual int32_t GetRoomIndexByHash(uint32_t hash) const = 0;
+		virtual std::string GetRoomName(uint32_t roomIndex) const = 0;
+		virtual int32_t GetRoomFlag(uint32_t roomIndex) const = 0;
+		virtual ExtentInfo GetRoomExtents(uint32_t roomId) const = 0;
+		virtual uint32_t GetRoomTimecycle(uint32_t roomIndex) const = 0;
+
+		virtual alt::Position GetPortalCornerPosition(uint32_t portalIndex, uint32_t cornerIndex) const = 0;
+		virtual int32_t GetPortalRoomFrom(uint32_t portalIndex) const = 0;
+		virtual int32_t GetPortalRoomTo(uint32_t portalIndex) const = 0;
+		virtual int32_t GetPortalFlag(uint32_t portalIndex) const = 0;
+
+		virtual uint16_t GetRoomCount() const = 0;
+		virtual uint16_t GetPortalCount() const = 0;
+		virtual alt::Position GetPosition() const = 0;
+		virtual alt::Position GetRotation() const = 0;
+		virtual alt::IInterior::ExtentInfo GetEntitiesExtents() const = 0;
+
+		virtual void SetRoomFlag(uint32_t roomIndex, uint32_t flag) = 0;
+		virtual void SetRoomTimecycle(uint32_t roomIndex, uint32_t timecycleHash) = 0;
+		virtual void SetRoomExtents(uint32_t roomIndex, ExtentInfo extentInfo) = 0;
+
+		virtual void SetPortalCornerPosition(uint32_t portalIndex, uint32_t cornerIndex,
+		                                     const alt::Position& position) = 0;
+		virtual void SetPortalRoomFrom(uint32_t portalIndex, uint32_t roomFrom) = 0;
+		virtual void SetPortalRoomTo(uint32_t portalIndex, uint32_t roomTo) = 0;
+		virtual void SetPortalFlag(uint32_t portalIndex, uint32_t flag) = 0;
+	};
+}
+
+//##endif
