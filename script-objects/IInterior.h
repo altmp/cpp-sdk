04087fadc3ea8a56c47a1802fa0ffb766ac1d6af
Till Schreiber <till@tillschreiber.de>
2024-10-04 10:57:48 +0200

    ALTV-426 Add more interior apis

--- script-objects/IInterior.h
+++ script-objects/IInterior.h
@@ -34,6 +34,12 @@ namespace alt
 		virtual alt::Position GetRotation() const = 0;
 		virtual alt::IInterior::ExtentInfo GetEntitiesExtents() const = 0;
 
+		virtual uint16_t GetPortalEntityCount(uint32_t portalIndex) const = 0;
+		virtual uint32_t GetPortalEntityArcheType(uint32_t portalIndex, uint32_t entityIndex) const = 0;
+		virtual int32_t GetPortalEntityFlag(uint32_t portalIndex, uint32_t entityIndex) const = 0;
+		virtual alt::Position GetPortalEntityPosition(uint32_t portalIndex, uint32_t entityIndex) const = 0;
+		virtual alt::Position GetPortalEntityRotation(uint32_t portalIndex, uint32_t entityIndex) const = 0;
+
 		virtual void SetRoomFlag(uint32_t roomIndex, uint32_t flag) = 0;
 		virtual void SetRoomTimecycle(uint32_t roomIndex, uint32_t timecycleHash) = 0;
 		virtual void SetRoomExtents(uint32_t roomIndex, ExtentInfo extentInfo) = 0;
@@ -43,6 +49,7 @@ namespace alt
 		virtual void SetPortalRoomFrom(uint32_t portalIndex, uint32_t roomFrom) = 0;
 		virtual void SetPortalRoomTo(uint32_t portalIndex, uint32_t roomTo) = 0;
 		virtual void SetPortalFlag(uint32_t portalIndex, uint32_t flag) = 0;
+		virtual void SetPortalEntityFlag(uint32_t portalIndex, uint32_t entityIndex, uint32_t flag) = 0;
 	};
 }
 
