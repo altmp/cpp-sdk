3979d30bfaf4fff129b1aff5c85cb7ff23fd496f
Till Schreiber <till@tillschreiber.de>
2025-01-18 21:15:56 +0100

    ALTV-640 - add new PARACHUTE_STATE_CHANGE Event

---
+++ events/CParachuteStateChangeEvent.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include "CEvent.h"
+
+namespace alt
+{
+	class CParachuteStateChangeEvent : public CEvent
+	{		
+	public:
+		enum class ParachuteState : int8_t
+		{
+			NORMAL = -1,
+			CARRING_PARACHUTE = 0,
+			OPENING_PARACHUTE = 1,
+			OPEN_PARACHUTE = 2,
+			REMOVE_PARACHUTE = 3
+		};
+		
+		CParachuteStateChangeEvent(const std::shared_ptr<IPlayer>& _player, ParachuteState _oldState, ParachuteState _newState) :
+			CEvent(Type::PARACHUTE_STATE_CHANGE),
+			player(_player), oldState(_oldState), newState(_newState)
+
+		{
+
+		}
+
+		IPlayer* GetPlayer() const { return player.get(); }
+		ParachuteState GetOldState() const { return oldState; }
+		ParachuteState GetNewState() const { return newState; }
+	private:		
+		std::shared_ptr<IPlayer> player;
+		ParachuteState oldState;
+		ParachuteState newState;
+	};
+}
